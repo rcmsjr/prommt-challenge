65189b10d6149f818ebe90ffd1c85bd106f5a450
Roberto Souza <rcmsjr@gmail.com>
2025-03-06 15:53:11 +0000

    feat(payment-service): enable CORS for localhost frontend app

--- apps/payment-service/src/main/java/com/server/payment/application/rest/controllers/PaymentController.java
+++ apps/payment-service/src/main/java/com/server/payment/application/rest/controllers/PaymentController.java
@@ -20,11 +20,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/payments")
+@CrossOrigin(origins = "http://localhost:4200")
 @Validated
 public class PaymentController {
 
